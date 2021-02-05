285d7da77311b5baa5ff31c5f3538504d783b650
Feyyaz Can KÖSE <56981145+feyyazcn19@users.noreply.github.com>
2021-02-06 02:49:42 +0300

    Add files via upload

---
+++ scripts/form-carousel_0830.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'Müşteri',
+    'email_message' => 'Müşterinizden yeni bir mesajın var.',
+    'success_redirect' => '',
+    'email' => array(
+    'from' => '',
+    'to' => 'kesemenere123@gmail.com'
+    ),
+    'fields' => array(
+    'email' => array(
+    'order' => 1,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'name' => array(
+    'order' => 2,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor();
+    $processor->process($form);
+
+    ?>
